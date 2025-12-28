191aef1013974cdfd9fc317e02575b5a25c21f6e
Dan36252 <danielabkairov@gmail.com>
2025-12-27 22:52:04 -0800

    tuning

--- src/Claw.py
+++ src/Claw.py
@@ -56,7 +56,7 @@ class Claw:
         offset = 0
         if push == False:
             if self.face == "D":
-                offset = 15
+                offset = -10
             else:
                 offset = -20
         angle = self.extended_angles[self.face]
