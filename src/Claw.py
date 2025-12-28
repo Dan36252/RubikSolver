e593ac2241b22f325b16fd1efea56f5a0c519973
Dan36252 <danielabkairov@gmail.com>
2025-12-27 22:26:55 -0800

    tuning extension

--- src/Claw.py
+++ src/Claw.py
@@ -9,10 +9,10 @@ class Claw:
         # Angles corresponding to the extended state of each claw.
         # These angles are when the claws fully push on the cube.
         self.extended_angles = {
-            "L": 65,
-            "F": 60,
-            "R": 40,
-            "B": 40,
+            "L": 70,
+            "F": 65,
+            "R": 45,
+            "B": 45,
             "D": 13
         }
 
@@ -58,7 +58,7 @@ class Claw:
             if self.face == "D":
                 offset = 5
             else:
-                offset = -15
+                offset = -20
         angle = self.extended_angles[self.face]
         self.extendor.angle = angle - offset
 
