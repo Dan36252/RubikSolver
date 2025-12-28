32017649c91a7649cc3ac5a1ac6387bdef42d11e
Dan36252 <danielabkairov@gmail.com>
2025-12-27 23:40:03 -0800

    New Claw Machine Test of solving a cube following a certain formula

--- src/ClawMachineTest.py
+++ src/ClawMachineTest.py
@@ -3,18 +3,7 @@ import time
 
 robot = ClawMachine()
 
-print("TEST 1")
-robot.move("L")
-time.sleep(3)
-print("TEST 2")
-robot.move("D'")
-time.sleep(3)
-print("TEST 3")
-robot.move("U2")
-time.sleep(3)
-print("TEST 4")
-robot.turn_cube("L")
-time.sleep(3)
-print("TEST 5")
-robot.turn_cube("D")
-time.sleep(3)
+formula = ["L2", "U", "D'", "U'", "R'"]
+
+for m in formula:
+    robot.move(m)
