faa6b7c0d000901845001705ed4b70223e31f199
Dan36252 <danielabkairov@gmail.com>
2026-03-09 00:26:47 -0700

    fixed shaped data bug, removed cube reading until made more accurate

--- src/Main.py
+++ src/Main.py
@@ -6,15 +6,32 @@ from DeepCubeA.environments.cube3 import Cube3
 
 print("Starting!")
 
+# Initializing Claw Machine
 robot = ClawMachine()
 
-reader = CubeReader(robot)
-cube_colors = reader.ReadCube()
+# Reading and reconstructing Cube
+# reader = CubeReader(robot)
+# face_colors = reader.ReadCube()
+#
+# # Flattening colors outputted by Cube Reader
+# flat_colors = []
+# for f in range(len(face_colors)):
+#     for c in range(len(face_colors[f])):
+#         flat_colors.append(shaped_data[f][c])
+#
+# print("Got reconstructed colors!")
+# print(flat_colors)
+# [1, 1, 0, 3, 1, 4, 5, 1, 3, 0, 1, 5, 5, 2, 5, 3, 3, 1, 5, 5, 2, 1, 4, 4, 2, 3, 2, 4, 2, 0, 2, 5, 5, 4, 3, 5, 4, 0, 0, 2, 3, 2, 3, 4, 3, 1, 0, 2, 0, 0, 0, 4, 4, 1]
 
-print("Got reconstructed colors!")
-print(cube_colors)
+# Temporary state generation, until Cube Reader is made more accurate
+scramble = ["R", "U", "R'", "L", "D", "B2", "F'", "D'", "R2", "F", "B2", "L'"]
+state = CubeState(encode=False)
+for s in scramble:
+    state.move(s)
+flat_colors = state.flat_data
 
-cubestate = CubeState(data=cube_colors, encode=False)
+# Preparing data for DeepCubeA
+cubestate = CubeState(data=flat_colors, encode=False)
 deep_cube_state = cubestate.get_deepcube_data()
 
 print("\n\nDeep Cube colors data:")
